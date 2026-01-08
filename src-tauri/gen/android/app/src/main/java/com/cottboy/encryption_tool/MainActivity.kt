ebaf140ad437c281d4feb927c4313e5121dc055e
cottboy <200020097+cottboy@users.noreply.github.com>
2026-01-08 22:26:00 +0800

    feat: 添加 Android 项目配置

---
+++ src-tauri/gen/android/app/src/main/java/com/cottboy/encryption_tool/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cottboy.encryption_tool
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
